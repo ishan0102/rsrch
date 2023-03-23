91ab66068676a981d88bb6e9835812cf39eda0c1
Ishan Shah <ishan0102@utexas.edu>
2023-03-23 00:56:00 -0500

    Works locally, papers can be sent from phone

--- src/bot.py
+++ src/bot.py
@@ -4,8 +4,11 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+from rsrch import push_papers
+
 load_dotenv()
 
+# Create intents to allow for message content
 intents = discord.Intents.default()
 intents.messages = True
 intents.guilds = True
@@ -13,21 +16,21 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-
+# Login sequence
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
 
+# Push papers to Notion
 @bot.event
 async def on_message(message):
-    if message.author == bot.user:
+    if message.author == bot.user or message.channel.name != "rsrch":
         return
 
-    # Your custom code to run on each message
-    print(f"Message received: {message.content}")
-
+    push_papers([message.content])
     await bot.process_commands(message)
 
 
+# Run the bot
 bot.run(os.getenv("DISCORD_BOT_TOKEN"))
