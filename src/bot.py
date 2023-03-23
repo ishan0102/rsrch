86ed20384e22bdbd04bcb95e1866853d65e61003
Ishan Shah <ishan0102@utexas.edu>
2023-03-23 01:35:27 -0500

    Capture stdout and send it as a Discord message

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,6 @@
 import os
+import io
+import sys
 
 import discord
 from discord.ext import commands
@@ -8,7 +10,6 @@ from rsrch import push_papers
 
 load_dotenv()
 
-# Create intents to allow for message content
 intents = discord.Intents.default()
 intents.messages = True
 intents.guilds = True
@@ -16,21 +17,39 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Login sequence
+
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
 
-# Push papers to Notion
 @bot.event
 async def on_message(message):
     if message.author == bot.user or message.channel.name != "rsrch":
         return
 
-    push_papers([message.content])
+    # Redirect stdout to a StringIO object
+    original_stdout = sys.stdout
+    sys.stdout = captured_stdout = io.StringIO()
+
+    # Call the push_papers function
+    # Split the message content into a list of arguments if there are more than one
+    if len(message.content.split()) > 1:
+        push_papers(message.content.split())
+    else:
+        push_papers([message.content])
+
+    # Reset stdout to the original value
+    sys.stdout = original_stdout
+
+    # Get the captured output and send it to the channel
+    output = captured_stdout.getvalue()
+    if output:
+        await message.channel.send(f"```\n{output}\n```")
+    else:
+        await message.channel.send("No output was captured.")
+
     await bot.process_commands(message)
 
 
-# Run the bot
 bot.run(os.getenv("DISCORD_BOT_TOKEN"))
